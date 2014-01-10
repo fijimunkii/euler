be2f89b9170326a80eaae7efbb35683ca9e0b78f
Harrison Powers <fijimunkii@gmail.com>
2014-01-10 01:35:09 -0500

    number 13 needs some work

--- euler12.rb
+++ euler12.rb
@@ -8,6 +8,8 @@ triangle = 0
 until num_divisors > 500
   triangle+=1
   num_divisors = (0..triangle).inject(0) { |res, num| res + num }.factors.count
+  p triangle
+  p num_divisors
 end
 
 p triangle
